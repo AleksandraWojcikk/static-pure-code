14405dfb777c49c8309abcdfd53a5b0c2f2a71f3
AleksandraWojcikk <aleksandra.wojcikk88@gmail.com>
2023-11-15 20:59:54 +0100

    ćwiczenia ze statycznej analizy kodu

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,58 @@
-
+import { useEffect, useState } from 'react';
 import './App.css';
+
+
 
 function App() {
 
-  // const a: number = 5;
-  // const b: number = 6;
+type HeaderProps = {
+  title: string;
+  numberInTitle: number;
+}
+type FooterProps = {
+  info: string;
+}
 
-  const add = (a: number, b: number) => a + b ;
-  console.log(add(7, 0));
+
+interface Character {
+  name: string;
+}
+
+
+  const Header = ({title, numberInTitle}: HeaderProps) => {
+    return (<h2>{title} {numberInTitle}</h2>);
+  }
+
+  const Footer =({info}: FooterProps) => {
+    return (<h2>{info}</h2>);
+  }
+
+  //Fetchowanie danych z zew. API
+ const [charakters, setCharakters] = useState<Character[]>([]);
+ 
+  useEffect(() => {
+  fetch('https://rickandmortyapi.com/api/character?page=1')
+  .then(response => response.json())
+  .then(data => setCharakters(data.results)) //jezeli nie nie ma wew. danych klucza results, to mozna zostawic samo data (bez .results)
+ },[])  
+
   
-
-  
-
-
-
-
-
-
 
 
   return (
     <>
-      <h1> Poćwicz TS </h1>
+    <Header title="Przykładowy nagłówek" numberInTitle={1} />
+      <h1> Statyczna analiza kodu </h1>
+      {/* wyświetlenie danych pobranych z API */}
+      {charakters.map((character)=>{
+          return(<div>
+            {character.name}
+          </div>)
+        })}
+
+
+
+      <Footer info="Przykładowy footer" />
       
 
 
